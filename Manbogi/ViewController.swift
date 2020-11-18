54bc9592b859c49e57cd3acf1c03b36d22cfce9b
w00suk1234 <w00suk1234@naver.com>
2020-11-18 23:08:24 +0900

    Initial Commit

---
+++ Manbogi/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Manbogi
+//
+//  Created by 이우석 on 2020/11/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
